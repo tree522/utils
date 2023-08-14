312492ddd103769f08ae18387fd6b0560e5b161a
tree522 <lemontree_c522@outlook.com>
2023-08-14 23:26:04 +0800

    炫酷多彩的按钮

--- vue2demo/src/App.vue
+++ vue2demo/src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <ColorfulButton></ColorfulButton>
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/ArrayUtils1.vue";
-
+import ColorfulButton from "./components/ColorfulButton.vue";
 export default {
   name: "App",
   components: {
     HelloWorld,
+    ColorfulButton,
   },
 };
 </script>
---
+++ vue2demo/src/components/ColorfulButton.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <div>多彩的按钮</div>
+    <a href="#" class="color-button">button</a>
+  </div>
+</template>
+<style scoped>
+.color-button {
+  position: relative;
+  display: inline-block;
+  width: 200px;
+  height: 60px;
+  line-height: 60px;
+  background-color: skyblue;
+  color: #fff;
+  text-align: center;
+  text-decoration: none;
+  background: linear-gradient(90deg, #03a9f4, #f441a5, #ffeb3b, #03a9f4);
+  background-size: 400%;
+  border-radius: 10px;
+}
+.color-button:hover {
+  animation: animate 8s linear infinite;
+}
+@keyframes animate {
+  0% {
+    background-position: 0 0;
+  }
+  100% {
+    background-position: 400% 0;
+  }
+}
+.color-button:before {
+  content: "";
+  position: absolute;
+  left: -5px;
+  top: -5px;
+  right: -5px;
+  bottom: -5px;
+  z-index: -1;
+  background: linear-gradient(90deg, #03a9f4, #f441a5, #ffeb3b, #03a9f4);
+  background-size: 400%;
+  border-radius: 10px;
+  opacity: 0;
+  transition: all 1s;
+}
+.color-button:hover:before {
+  filter: blur(20px);
+  opacity: 1;
+}
+</style>
